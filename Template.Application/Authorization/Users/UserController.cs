79dbb871b568c02723e18e38b7e76544dc9451b0
Armen1998 <armena1998@yahoo.com>
2019-02-10 19:09:53 +0400

    hh

---
+++ Template.Application/Authorization/Users/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Application.Authorization.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "user1", "user2" };
+        }
+
+    }
+}
