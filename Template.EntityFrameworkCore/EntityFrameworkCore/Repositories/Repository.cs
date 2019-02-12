5005a018387e75481b10c098621610ac32bc3234
Armen1998 <armena1998@yahoo.com>
2019-02-13 00:59:13 +0400

    Add logic of reposiory

---
+++ Template.EntityFrameworkCore/EntityFrameworkCore/Repositories/Repository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Template.Core;
+
+namespace Template.EntityFrameworkCore.Repositories
+{
+    public class Repository<TEntity, TPrimaryKey> : IRepository<TEntity, TPrimaryKey>
+      where TEntity : class, IEntity
+    {
+        private readonly TemplateDbContext _context;
+        private DbSet<TEntity> _entity;
+
+        public Repository(
+            TemplateDbContext context
+            )
+        {
+            _context = context;
+        }
+
+        protected virtual DbSet<TEntity> Entity => _context.Set<TEntity>();
+
+        public void Delete(TPrimaryKey id)
+        {
+            Entity.Remove(Entity.Find(id));
+            _context.SaveChanges();
+        }
+
+        public void Delete(Expression<Func<TEntity, bool>> predicate)
+        {
+            Entity.RemoveRange(Entity.Where(predicate));
+            _context.SaveChanges();
+        }
+
+        public async Task DeleteAsync(TPrimaryKey id)
+        {
+            Entity.Remove(Entity.Find(id));
+            await _context.SaveChangesAsync();
+        }
+
+        public async void DeleteAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            Entity.RemoveRange(Entity.Where(predicate));
+            await _context.SaveChangesAsync();
+        }
+
+        public TEntity Get(TPrimaryKey id)
+        {
+            return Entity.Find(id);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return Entity;
+        }
+
+        public void Insert(TEntity entity)
+        {           
+            Entity.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task InsertAsync(TEntity entity)
+        {
+            Entity.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
