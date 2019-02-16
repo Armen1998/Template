a73b8ff7f766497710fe80d49ac63c4b178bbc08
Armen1998 <armena1998@yahoo.com>
2019-02-16 22:01:59 +0400

    Intitial create for user and role manager classes.

---
+++ Template.Core/Authorization/Users/IUserManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Core.Dependency;
+
+namespace Template.Core.Authorization.Users
+{
+    public interface IUserManager : ITransientDependency
+    {
+    }
+}
---
+++ Template.Core/Authorization/Users/UserManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Core.Dependency;
+
+namespace Template.Core.Authorization.Users
+{
+    public class UserManager : IUserManager, ITransientDependency
+    {
+    }
+}
