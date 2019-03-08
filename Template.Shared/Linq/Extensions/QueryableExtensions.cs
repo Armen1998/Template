c86c3da8d503c57901b1bbb21f08bc2433ec6fe2
Armen1998 <armena1998@yahoo.com>
2019-03-08 21:16:42 +0400

    create Template.Shared project

---
+++ Template.Shared/Linq/Extensions/QueryableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Template.Shared.Linq.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IEnumerable<TSource> WhereIf<TSource>(this IEnumerable<TSource> source, bool condition, Func<TSource, bool> predicate)
+        {
+            if (!condition)
+                return source;
+            else
+                return source
+                    .Where(predicate);
+        }
+
+        public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> source, bool condition, Expression<Func<TSource, bool>> predicate)
+        {  
+            if (!condition)
+                return source;
+            else
+                return source
+                    .Where(predicate);
+        }
+    }
+}
