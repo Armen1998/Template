c4d0491935a36ed9ec2ae38e3238c4fe37f9741a
Armen1998 <armena1998@yahoo.com>
2019-03-09 21:54:52 +0400

    create DefaultRoleBuilder and DefaultUserBuilder classes.

---
+++ Template.EntityFrameworkCore/EntityFrameworkCore/Seed/DefaultRoleBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.EntityFrameworkCore.EntityFrameworkCore.Seed
+{
+    public class DefaultRoleBuilder
+    {
+    }
+}
