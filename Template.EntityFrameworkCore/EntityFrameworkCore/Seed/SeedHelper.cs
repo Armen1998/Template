616dbb6861c907c1e403e6c1245b49fd4c56db1d
Armen1998 <armena1998@yahoo.com>
2019-03-09 21:55:16 +0400

    create SeedHelper class.

---
+++ Template.EntityFrameworkCore/EntityFrameworkCore/Seed/SeedHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.EntityFrameworkCore.EntityFrameworkCore.Seed
+{
+    public class SeedHelper
+    {
+        public static void SeedDb(TemplateDbContext context)
+        {
+            
+        }
+    }
+}
