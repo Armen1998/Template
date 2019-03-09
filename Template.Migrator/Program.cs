3341862fa025eb72ca8fc8f499f404faddc3db68
Armen1998 <armena1998@yahoo.com>
2019-03-09 21:46:51 +0400

    create Template.Migrator project for migrate db.

---
+++ Template.Migrator/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Core.Configuration;
+using Template.EntityFrameworkCore;
+using Template.EntityFrameworkCore.EntityFrameworkCore.Seed;
+using Template.Shared.Extensions.Reflection;
+
+namespace Template.Migrator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var service = new ServiceCollection();
+            var _configuration = AppConfiguration.Get(
+                 typeof(Program).GetAssembly().GetDirectoryPathOrNull()
+                );
+
+            service.AddDbContext<TemplateDbContext>(opts =>
+               opts.UseNpgsql(_configuration.GetConnectionString("Default")));
+
+            service.AddSingleton<TemplateDbContext>();
+
+            var context = service
+                .BuildServiceProvider()
+                .GetService<TemplateDbContext>();
+
+            SeedHelper.SeedDb(context);
+        }
+    }
+}
