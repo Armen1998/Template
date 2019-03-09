5abe5c0479113854bdc420ecf4f3f4b013b6058e
Armen1998 <armena1998@yahoo.com>
2019-03-09 21:51:04 +0400

    create AppConfiguration class, that returned configuration.

---
+++ Template.Core/Configuration/AppConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Template.Core.Configuration
+{
+    public class AppConfiguration
+    {
+        public static IConfigurationRoot Get(string path, string environmentName = null)
+        {
+            return BuildConfiguration(path, environmentName);
+        }
+
+        private static IConfigurationRoot BuildConfiguration(string path, string environmentName = null)
+        {
+            var builder = new ConfigurationBuilder()
+                   .SetBasePath(path)
+                   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            if (!string.IsNullOrWhiteSpace(environmentName))
+            {
+                builder = builder.AddJsonFile($"appsettings.{environmentName}.json", optional: true);
+            }
+
+            builder = builder.AddEnvironmentVariables();
+            
+            return builder.Build();
+        }
+    }
+}
